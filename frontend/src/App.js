42cdaa3dc9de02ef00b82cf06ff85c89c1bc2eea
Sabrina Peng <shpeng@smu.edu>
2020-06-09 18:50:54 -0500

    basic form created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import values from 'lodash/values';
 import JobPosting from './components/JobPosting.js';
+import { Container, Col, Form, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -15,15 +16,49 @@ function App() {
         setJobs(values(response.data.jobs));
       });
     };
-    
+
     getData();
   }, []);
 
+  let search = {
+    "location": [45.123, 47.232],
+    "name": "developer",
+    "industry": "tech",
+    "posted_after": 20200404
+  };
+
   return (
     <div className="App">
-      <div className="jobs-grid">
-        {jobs.map((job, i) => <div className="col"><JobPosting key={i} job={job} /></div>)}
-      </div>
+      <Container fluid>
+        <Form className="search-form">
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Keywords</Form.Label>
+              <Form.Control type="text" placeholder="Search by title or skill" />
+            </Form.Group>
+            <Form.Group as={Col}>
+                <Form.Label>Employer</Form.Label>
+                <Form.Control type="text" placeholder="Example: Google" />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Date Posted</Form.Label>
+              <Form.Control type="text" placeholder="Example: 4 days ago" />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Distance (Radius)</Form.Label>
+              <Form.Control type="text" placeholder="Example: 15 miles" />
+            </Form.Group>
+            <Form.Group as={Col} xs="auto">
+              <Button type="submit" className="align-self-end">Search</Button>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Container>
+      <Container fluid>
+        <div className="jobs-grid">
+          {jobs.map((job, i) => <div className="col"><JobPosting key={i} job={job} /></div>)}
+        </div>
+      </Container>
     </div>
   );
 }
