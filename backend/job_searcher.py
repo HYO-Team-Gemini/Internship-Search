8963285782177f19100a428dc88b9ff1b14292e3
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-10 02:41:41 -0400

    User Location From IP
    Requires new entry in credentials.json for IPStack request to work.

--- backend/job_searcher.py
+++ backend/job_searcher.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 import marshmallow
 import pymongo
+import urllib3
 from bson import json_util
 
 credentials = json.load(open('backend/credentials.json'))
@@ -11,7 +12,9 @@ password = credentials['MongoDB']['Password']
 client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@ekko-test-qbczn.mongodb.net/jobs?retryWrites=true&w=majority")
 db = client.jobs
 
-def search(args: dict) -> dict:
+http = urllib3.PoolManager()
+
+def search(args: dict, remote_address: str) -> dict:
     query_args = mongo_query_args(args)
     filtered_jobs = list(db.jobs.find(query_args))
     jobs = {}
@@ -27,12 +30,19 @@ def search(args: dict) -> dict:
 def mongo_query_args(args: dict) -> dict:
     query_args = {}
     for key in args.keys():
-        if args[key] != '*':
+        if args[key] != '*' and (key == 'name' or key == 'employer'):
             query_args[key] = {
                 "$regex": f'.*{args[key]}.*',
                 "$options" :'i'
-            }
+            }  
     return query_args
 
 def sanitize_mongo_bson(inputMongo: dict) -> dict:
     return json.loads(json_util.dumps(inputMongo))
+
+def get_user_coordinates(ip:str) -> list:
+    api_key = credentials["IPStack"]["Access Key"]
+    api_address = f"http://api.ipstack.com/{ip}?access_key={api_key}"
+    result = json.loads(http.request('GET', api_address).data)
+    return [result['latitude'], result['longitude']]
+
