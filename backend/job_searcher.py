8cf3a0e08f7e81c830074cad21652cdb42f21371
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-10 14:56:22 -0400

    Location Query Basics

--- backend/job_searcher.py
+++ backend/job_searcher.py
@@ -15,34 +15,51 @@ db = client.jobs
 http = urllib3.PoolManager()
 
 def search(args: dict, remote_address: str) -> dict:
-    query_args = mongo_query_args(args)
-    filtered_jobs = list(db.jobs.find(query_args))
+    results = []
+    query_args = mongo_query_args(args, remote_address)
+    results = list(db.jobs.find(query_args))
     jobs = {}
-    for job in filtered_jobs:
+    for job in results:
         job = sanitize_mongo_bson(job)
         jobs[str(len(jobs.keys()) + 1)] = job
-    output = { "num_jobs": len(filtered_jobs), "jobs": jobs}
+    output = { "num_jobs": len(results), "jobs": jobs}
     print('Queried for:')
     pprint(args)
     pprint(output)
     return output
 
-def mongo_query_args(args: dict) -> dict:
+def mongo_query_args(args: dict, ip: str) -> dict:
     query_args = {}
     for key in args.keys():
         if args[key] != '*' and (key == 'name' or key == 'employer'):
             query_args[key] = {
                 "$regex": f'.*{args[key]}.*',
                 "$options" :'i'
-            }  
+            }
+    if args['distance'] != 0:
+        query_args['location'] = build_location_query(args, ip)
     return query_args
 
 def sanitize_mongo_bson(inputMongo: dict) -> dict:
     return json.loads(json_util.dumps(inputMongo))
 
-def get_user_coordinates(ip:str) -> list:
+def get_user_coordinates(ip: str) -> list:
     api_key = credentials["IPStack"]["Access Key"]
     api_address = f"http://api.ipstack.com/{ip}?access_key={api_key}"
     result = json.loads(http.request('GET', api_address).data)
     return [result['latitude'], result['longitude']]
 
+def build_location_query(args: dict, ip: str) -> dict:
+    return { 
+        '$near' : {
+            '$geometry': { 
+                'type': "Point",
+                'coordinates': get_user_coordinates(ip) 
+            },
+            '$minDistance': 0,
+            '$maxDistance': 1609.34 * args['distance']
+        }
+    }
+    
+
+search({'distance': 5, 'max_returns': 50}, '73.245.3.190')
