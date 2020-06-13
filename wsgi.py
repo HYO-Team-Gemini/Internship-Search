84d973619d8f92f1bc6e6c4ef29d84918a3c39df
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-12 22:02:06 -0400

    Configure To Use Heroku Env Variable

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
+import os
+
 from waitress import serve
-from backend.api import app 
-  
-if __name__ == "__main__": 
-    serve(app)
+
+from backend.api import app
+
+if __name__ == "__main__":
+    try:
+        port = os.getenv('PORT')
+    except:
+        port = 8080
+    serve(app, port=port)
