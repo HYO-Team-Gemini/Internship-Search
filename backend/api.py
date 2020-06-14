7e660fb15e91892bab3ff79bc34f07964bac65a7
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-14 18:43:28 -0400

    Catch All For Runtime Errors In Code

--- backend/api.py
+++ backend/api.py
@@ -14,7 +14,7 @@ parser.add_argument('employer', default='*', type=str)
 parser.add_argument('distance', default=0, type=int)
 parser.add_argument('page', default=1, type=int)
 parser.add_argument('max_returns', default=50, type=int)
-parser.add_argument('zipcode', default='*', type=int)
+parser.add_argument('zipcode', default=0, type=int)
 parser.add_argument('city', default='*', type=str)
 parser.add_argument('state', default='*', type=str)
 
@@ -24,8 +24,10 @@ class JobList(Resource):
     def get(self):
         args = parser.parse_args()
         user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-        print(user_ip)
-        output = job_searcher.search(args, user_ip)
+        try:
+            output = job_searcher.search(args, user_ip)
+        except:
+            output = {'message': 'Error Occurred Somewhere'}
         return output
 
 ##
