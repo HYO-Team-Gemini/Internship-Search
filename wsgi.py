279be8245fcccf2ad7850597d2f613ea5fb72b56
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-14 00:54:34 -0400

    Generate Zipcode Database On Server Start

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 import os
 
+from uszipcode import SearchEngine
 from waitress import serve
 
 from backend.api import app
+
+search = SearchEngine(db_file_dir="backend/tmp")
 
 if __name__ == "__main__":
     try:
