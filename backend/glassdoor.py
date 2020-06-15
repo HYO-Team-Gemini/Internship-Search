e5dcdcec9c438ffba4f5dfdf00d86bd5285721f9
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-15 19:33:33 -0400

    Refactor File
    The location ID request should have been happening in a different method.
    
    The location ID request now will raise an HTTP error when it fails, that might be partially handled by the server in the future.
    
    In general the code should be much cleaner and easier to read without raising a false exeception.

--- backend/glassdoor.py
+++ backend/glassdoor.py
@@ -7,24 +7,13 @@ import sys
 import urllib.request
 from pprint import pprint
 
-from bs4 import BeautifulSoup
 import requests
 import unicodecsv as csv
+from bs4 import BeautifulSoup
+from flask_restful import HTTPException
 from lxml import etree, html
 
-
-def scrape(keyword: str = 'job', place: str = 'us'):
-
-	headers = {	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-				'accept-encoding': 'gzip, deflate, sdch, br',
-				'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
-				'referer': 'https://www.glassdoor.com/',
-				'upgrade-insecure-requests': '1',
-				'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-				'Cache-Control': 'no-cache',
-				'Connection': 'keep-alive'
-	}
-
+def get_place_id(place: str) -> str:
 	location_headers = {
 		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.01',
 		'accept-encoding': 'gzip, deflate, sdch, br',
@@ -35,79 +24,97 @@ def scrape(keyword: str = 'job', place: str = 'us'):
 		'Cache-Control': 'no-cache',
 		'Connection': 'keep-alive'
 	}
-	data = {"term": place,
-			"maxLocationsToReturn": 10}
+
+	data = {
+		"term": place,
+		"maxLocationsToReturn": 10
+	}
 
 	location_url = "https://www.glassdoor.co.in/findPopularLocationAjax.htm?"
+
 	try:
-		# Getting location id for search location
 		location_response = requests.post(location_url, headers=location_headers, data=data).json()
 		place_id = location_response[0]['locationId']
-		job_litsting_url = 'https://www.glassdoor.com/Job/jobs.htm'
-		# Form data to get job results
-		data = {
-			'clickSource': 'searchBtn',
-			'sc.keyword': keyword,
-			'locT': 'C',
-			'locId': place_id,
-			'jobType': ''
-		}
-
-		job_listings = []
-		if place_id:
-			response = requests.post(job_litsting_url, headers=headers, data=data)
-			# extracting data from
-			# https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=andr&sc.keyword=android+developer&locT=C&locId=1146821&jobType=
-			parser = html.fromstring(response.text)
-			# Making absolute url
-			base_url = "https://www.glassdoor.com"
-			parser.make_links_absolute(base_url)
-
-			XPATH_ALL_JOB = '//li[@class="jl"]'
-			XPATH_NAME = './/a/text()'
-			XPATH_JOB_URL = './/a/@href'
-			XPATH_LOC = './/span[@class="subtle loc"]/text()'
-			XPATH_COMPANY = './/div[@class="flexbox empLoc"]/div/text()'
-			XPATH_SALARY = './/span[@class="green small"]/text()'
-
-			listings = parser.xpath(XPATH_ALL_JOB)
-			for job in listings:
-				raw_job_name = job.xpath(XPATH_NAME)
-				raw_job_url = job.xpath(XPATH_JOB_URL)
-				raw_job_url = job.xpath(raw_job_url)
-				raw_lob_loc = job.xpath(XPATH_LOC)
-				raw_company = job.xpath(XPATH_COMPANY)
-				raw_salary = job.xpath(XPATH_SALARY)
-
-				# Cleaning data
-                #getting rid of chars like -
-				job_name = ''.join(raw_job_name).strip('–') if raw_job_name else None
-				job_location = ''.join(raw_lob_loc) if raw_lob_loc else None
-				raw_state = re.findall(",\s?(.*)\s?", job_location)
-				state = ''.join(raw_state).strip()
-				raw_city = job_location.replace(state, '')
-				city = raw_city.replace(',', '').strip()
-				company = ''.join(raw_company).replace('–','')
-				salary = ''.join(raw_salary).strip()
-				job_url = raw_job_url[0] if raw_job_url else None
-
-				jobs = {
-					"name": job_name,
-					"employer": company,
-					"state": state,
-					"city": city,
-					"salary": salary,
-					"link": job_url,
-					"date": datetime.datetime.utcnow()
-				}
-				job_listings.append(jobs)
-
-			return job_listings
-		else:
-			print("location id not available")
-
 	except:
-		print("Failed to load locations")
+		raise HTTPException(description=f'Error Loading Glassdoor Location Data For: {place}', response=500)
+
+	return place_id
+
+def scrape(keyword: str = 'job', place: str = 'us') -> list:
+
+	place_id = get_place_id(place)
+
+	job_litsting_url = 'https://www.glassdoor.com/Job/jobs.htm'
+	# Form data to get job results
+	data = {
+		'clickSource': 'searchBtn',
+		'sc.keyword': keyword,
+		'locT': 'C',
+		'locId': place_id,
+		'jobType': ''
+	}
+
+	headers = {	
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'accept-encoding': 'gzip, deflate, sdch, br',
+		'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
+		'referer': 'https://www.glassdoor.com/',
+		'upgrade-insecure-requests': '1',
+		'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	}
+	response = requests.post(job_litsting_url, headers=headers, data=data)
+	# extracting data from
+	# https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=andr&sc.keyword=android+developer&locT=C&locId=1146821&jobType=
+	parser = html.fromstring(response.text)
+	# Making absolute url
+	base_url = "https://www.glassdoor.com"
+	parser.make_links_absolute(base_url)
+
+	XPATH_ALL_JOB = '//li[@class="jl"]'
+	XPATH_NAME = './/a/text()'
+	XPATH_JOB_URL = './/a/@href'
+	XPATH_LOC = './/span[@class="subtle loc"]/text()'
+	XPATH_COMPANY = './/div[@class="flexbox empLoc"]/div/text()'
+	XPATH_SALARY = './/span[@class="green small"]/text()'
+
+	listings = parser.xpath(XPATH_ALL_JOB)
+
+	job_listings = []
+	
+	for job in listings:
+		raw_job_name = job.xpath(XPATH_NAME)
+		raw_job_url = job.xpath(XPATH_JOB_URL)
+		raw_lob_loc = job.xpath(XPATH_LOC)
+		raw_company = job.xpath(XPATH_COMPANY)
+		raw_salary = job.xpath(XPATH_SALARY)
+
+		# Cleaning data
+		#getting rid of chars like -
+		job_name = ''.join(raw_job_name).strip('–') if raw_job_name else None
+		job_location = ''.join(raw_lob_loc) if raw_lob_loc else None
+		raw_state = re.findall(",\s?(.*)\s?", job_location)
+		state = ''.join(raw_state).strip()
+		raw_city = job_location.replace(state, '')
+		city = raw_city.replace(',', '').strip()
+		company = ''.join(raw_company).replace('–','')
+		salary = ''.join(raw_salary).strip()
+		job_url = raw_job_url[0] if raw_job_url else None
+
+		job_cleaned = {
+			"name": job_name,
+			"employer": company,
+			"state": state,
+			"city": city,
+			"salary": salary,
+			"link": job_url,
+			"date": datetime.datetime.utcnow()
+		}
+		
+		job_listings.append(job_cleaned)
+
+	return job_listings
 
 if __name__ == "__main__":
-	print(scrape())
+	pprint(scrape())
