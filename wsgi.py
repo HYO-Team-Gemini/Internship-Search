1c204569ef79e49a653cee08b4dd3a47ddc0a7cc
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-15 01:37:06 -0400

    Patch For Local Testing

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,7 @@ search = SearchEngine(db_file_dir="backend/tmp")
 if __name__ == "__main__":
     try:
         port = os.getenv('PORT')
+        serve(app, port=port)
     except:
         port = 8080
-    serve(app, port=port)
+        serve(app, port=port)
