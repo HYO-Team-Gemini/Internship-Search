e74c8aef877f0121ba3b46a95a0d555d48bfaf1a
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-19 01:14:17 -0400

    Error Logging

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,8 @@
 import json
 import traceback
+from datetime import datetime
 
+import pymongo
 from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Api, HTTPException, Resource, abort, reqparse
@@ -10,30 +12,37 @@ if __name__ == "__main__":
 else:
     from backend import job_searcher
 
+credentials = json.load(open('backend/credentials.json'))
+username = credentials['MongoDB']['Username']
+password = credentials['MongoDB']['Password']
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app, catch_all_404s=True)
 
-parser = reqparse.RequestParser()
-parser.add_argument('name', default='*', type=str)
-parser.add_argument('employer', default='*', type=str)
-parser.add_argument('distance', default=0, type=int)
-parser.add_argument('page', default=1, type=int)
-parser.add_argument('max_returns', default=50, type=int)
-parser.add_argument('zipcode', default=0, type=int)
-parser.add_argument('city', default='*', type=str)
-parser.add_argument('state', default='*', type=str)
+joblist_parser = reqparse.RequestParser()
+joblist_parser.add_argument('name', default='*', type=str)
+joblist_parser.add_argument('employer', default='*', type=str)
+joblist_parser.add_argument('distance', default=0, type=int)
+joblist_parser.add_argument('page', default=1, type=int)
+joblist_parser.add_argument('max_returns', default=50, type=int)
+joblist_parser.add_argument('zipcode', default=0, type=int)
+joblist_parser.add_argument('city', default='*', type=str)
+joblist_parser.add_argument('state', default='*', type=str)
 
 # JobList
 # shows a list of all jobs
 class JobList(Resource):
     def get(self):
-        args = parser.parse_args()
+        args = joblist_parser.parse_args()
         try:
             return job_searcher.search(args), 200
         except HTTPException as e:
             abort(e.code, message=e.description)
         except Exception as e:
+            client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@ekko-test-qbczn.mongodb.net/jobs?retryWrites=true&w=majority")
+            client.errors.log.insert_one({'error': str(e), 'args': args, 'date': datetime.utcnow()})
+            client.close()
             abort(500, message=f"Unexpected Server Error Occurred: {str(e)}")
 
 ##
