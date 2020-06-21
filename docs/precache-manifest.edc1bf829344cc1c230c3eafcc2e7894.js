019c74077ebd5273ad006718ed4adeab4a999f4a
Sabrina Peng <shpeng@smu.edu>
2020-06-21 12:35:59 -0500

    remove gh-pages and use master/docs method for deployment

---
+++ docs/precache-manifest.edc1bf829344cc1c230c3eafcc2e7894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9def610126fdf162f24bdff35e4740e4",
+    "url": "/Internship-Search/index.html"
+  },
+  {
+    "revision": "0b312bedd57bf19561c9",
+    "url": "/Internship-Search/static/css/main.0b09b401.chunk.css"
+  },
+  {
+    "revision": "bc60038f1e382d3416f4",
+    "url": "/Internship-Search/static/js/2.733ac58d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Internship-Search/static/js/2.733ac58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b312bedd57bf19561c9",
+    "url": "/Internship-Search/static/js/main.ff4e52ce.chunk.js"
+  },
+  {
+    "revision": "5ecea0d6177fc6c24186",
+    "url": "/Internship-Search/static/js/runtime-main.0aabea67.js"
+  }
+]);
