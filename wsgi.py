5c6387bebaf7334b44c86afb44fa45e3c06305ab
EKKOING <nicholas.lorentzen@gmail.com>
2020-06-21 01:46:24 -0400

    Allow Debugging

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,7 @@ search = SearchEngine(db_file_dir="backend/tmp")
 if __name__ == "__main__":
     try:
         port = os.getenv('PORT')
-        serve(app, port=port)
+        serve(app, port=port, expose_tracebacks=True)
     except:
         port = 8080
         serve(app, port=port)
